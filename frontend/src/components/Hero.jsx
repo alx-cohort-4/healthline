5a457463f04300bac3dc057aa5467997cc51b390
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-05-02 00:13:27 +0000

    refactor: restructure application layout and styles
    - Removed App.css and integrated Tailwind CSS for styling.
    - Updated App component to use React Router for navigation.
    - Created AuthLayout component to manage authentication-related routes.
    - Added Header component for navigation links.
    - Introduced Button component with variants for consistent styling.
    - Implemented utility functions for class name management and button variants.
    - Updated main entry point to include BrowserRouter for routing.
    - Cleaned up unused styles and files to streamline the project structure.

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,7 +2,7 @@ const HeroSection = () => {
   return (
     <section>
       <h1>Hero Section</h1>
-      <p>This is a Hero section</p>
+      <p>This is a Hero Kofi</p>
     </section>
   );
 };
---
+++ frontend/src/components/shared/Header.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+
+const navItems = [
+  { name: "Features", path: "/features" },
+  { name: "How It Works", path: "/how-it-works" },
+  { name: "Contact Us", path: "/contact-us" },
+];
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center py-6   ">
+      <span className="text-primary text-3xl font-semibold ">
+        AI HealthLine
+      </span>
+      <nav>
+        <ul className="flex space-x-8">
+          {navItems.map((item) => (
+            <li key={item.name} className="text-primary ">
+              <Link to={item.path} className="">
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
