be06ca4c58607cc988e0698341095700c1fd0fc8
seunosinowo <oluwaseunpaul98@gmail.com>
2025-05-03 13:02:17 +0100

    Fix files in their respective folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,12 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import AuthLayout from "./components/Layout/AuthLayout";
-import Button from "./components/ui/Button";
-import EmailConfirmation from "./pages/EmailConfirmation";
+import React from 'react';
+import Nav from './pages/Nav';
+import HeroSection from './pages/HeroSection';
 
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Button>Home</Button>} />
-          <Route path="features" element={<div>Features</div>} />
-          <Route path="how-it-works" element={<div>How It Works</div>} />
-          <Route path="contact-us" element={<div>Contact Us</div>} />
-          <Route path="/email-confirmation" element={<EmailConfirmation />} />
-        </Route>
-      </Routes>
+    <div className="min-h-screen">
+      <Nav />
+      <HeroSection />
     </div>
   );
 };
