b7401b63e6961b4cf55eb6f7779533f00366d3db
seunosinowo <oluwaseunpaul98@gmail.com>
2025-05-03 11:09:50 +0100

    Setting up Frontend packages

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="w-full min-h-screen bg-gradient-to-r from-blue-500 to-blue-700 text-white flex flex-col items-center justify-center px-4">
+      
+    </div>
+  );
+};
+
+export default Hero; 
