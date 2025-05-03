8e3f4507a361b91e717bccd33fa2604c0883ec92
seunosinowo <oluwaseunpaul98@gmail.com>
2025-05-03 12:31:29 +0100

    Hero section Implemnented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from './components/Nav';
+import HeroSection from './components/HeroSection';
 
 const App = () => {
   return (
     <div>
       <Nav />
+      <HeroSection/>
     </div>
   );
 };
