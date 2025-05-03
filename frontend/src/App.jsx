9c3b200581a960e26199bc66a69bfb6361d118c0
seunosinowo <oluwaseunpaul98@gmail.com>
2025-05-03 13:12:10 +0100

    Added Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Nav from './pages/Nav';
 import HeroSection from './pages/HeroSection';
 
 const App = () => {
   return (
-    <div className="min-h-screen">
-      <Nav />
-      <HeroSection />
-    </div>
+    <Router>
+      <div className="min-h-screen">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Navigate to="/hero" replace />} />
+          <Route path="/hero" element={<HeroSection />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
