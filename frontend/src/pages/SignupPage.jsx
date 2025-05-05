401b85e7c31390753674a61547c12828e6b196bd
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-05-05 23:41:56 +0000

    feat: add signup functionality with form validation and UI components

---
+++ frontend/src/pages/SignupPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Signup from "../components/signup";
+
+const SignupPage = () => {
+  return (
+    <div className="flex items-center justify-center h-auto">
+      <Signup />
+    </div>
+  );
+};
+
+export default SignupPage;
