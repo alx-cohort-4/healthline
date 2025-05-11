bb51fb842e1c31827bbeb43dbbd52180da6a77d8
Grandkojo <essienernest.kojoowusu@gmail.com>
2025-05-11 21:28:34 +0000

    minor

--- tenant/token.py
+++ tenant/token.py
@@ -6,22 +6,22 @@ from django.template.loader import render_to_string
 from dotenv import load_dotenv
 load_dotenv()
 
-# # def expiration_time(minutes=0):
-# #     now = datetime.now(timezone.utc) + timedelta(minutes=minutes)
-# #     return int(now.timestamp())
+def expiration_time(minutes=0):
+    now = datetime.now(timezone.utc) + timedelta(minutes=minutes)
+    return int(now.timestamp())
 
-# def send_token_to_verify_email(user):
-#     now = int(datetime.now(timezone.utc).timestamp())
-#     expiry_time = expiration_time(int(os.getenv("TOKEN_EXPIRATION")))
-#     PAYLOAD = {
-#         'iat': now,
-#         'exp': expiry_time,
-#         'sub': user
-#     }
-#     with open('private.pem', 'r') as file:
-#         private_key = file.read()
-#     token = jwt.encode(payload=PAYLOAD, key=private_key, algorithm=os.getenv("ALGO"))
-#     return token
+def send_token_to_verify_email(user):
+    now = int(datetime.now(timezone.utc).timestamp())
+    expiry_time = expiration_time(int(os.getenv("TOKEN_EXPIRATION")))
+    PAYLOAD = {
+        'iat': now,
+        'exp': expiry_time,
+        'sub': user
+    }
+    with open('private.pem', 'r') as file:
+        private_key = file.read()
+    token = jwt.encode(payload=PAYLOAD, key=private_key, algorithm=os.getenv("ALGO"))
+    return token
 
 def decode_token(token):
     with open('public.pem', 'r') as f:
@@ -39,15 +39,6 @@ def decode_token(token):
         print(e)
         return None
 
-<<<<<<< HEAD
-# def send_email(email, user):
-#     token = send_token_to_verify_email(user)
-#     subject = "Email Verification"
-#     html_content = render_to_string(
-#         "tenant/email_verification.html",
-#         {"email": email, "token": token}
-#     )
-=======
 def send_email(email, user):
     token = send_token_to_verify_email(user)
     subject = "Email Verification"
@@ -56,38 +47,14 @@ def send_email(email, user):
         template_name="api/verify_email.html",
         context={"email": email, "verification_url": token_link}
     )
->>>>>>> 63a7ac6b2b2e14e26ddc052acd4a990db8723dc9
 
-#     msg = EmailMultiAlternatives(
-#         subject=subject,
-#         from_email=os.getenv("EMAIL_HOST_USER"),
-#         to=[email]
-#     )
-#     msg.attach_alternative(html_content, "text/html")
+    msg = EmailMultiAlternatives(
+        subject=subject,
+        from_email=os.getenv("EMAIL_HOST_USER"),
+        to=[email]
+    )
+    msg.attach_alternative(html_content, "text/html")
 
-<<<<<<< HEAD
-#     try:
-#         msg.send()
-#     except SMTPAuthenticationError:
-#         print("SMTP Authentication failed. Check your email credentials.")
-#     except SMTPConnectError:
-#         print("Failed to connect to the SMTP server. Is it reachable?")
-#     except SMTPRecipientsRefused:
-#         print("Recipient address was refused by the server.")
-#     except SMTPSenderRefused:
-#         print("Sender address was refused by the server.")
-#     except SMTPDataError:
-#         print("SMTP server replied with an unexpected error code (data issue).")
-#     except SMTPException as e:
-#         print(f"SMTP error occurred: {e}")
-#     except socket.gaierror:
-#         print("Network error: Unable to resolve SMTP server.")
-#     except socket.timeout:
-#         print("Network error: SMTP server timed out.")
-#     except Exception as e:
-#         print(f"An unexpected error occurred: {e}")
-
-=======
     try:
         msg.send()
     except SMTPAuthenticationError:
@@ -117,7 +84,6 @@ def send_email(email, user):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
         return None
->>>>>>> 63a7ac6b2b2e14e26ddc052acd4a990db8723dc9
 
 def send_reset_password_token(email, user):
     token = send_token_to_verify_email(user)
