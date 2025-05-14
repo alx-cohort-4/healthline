5e1ba32cb7333f0a01ec8a7b434a06a675e56d5d
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2025-05-14 09:54:49 +0000

    fix: adjust layout properties for improved responsiveness in Hero component

--- frontend/src/components/landing-page/Hero.jsx
+++ frontend/src/components/landing-page/Hero.jsx
@@ -4,7 +4,7 @@ import Button from "../ui/Button";
 const Hero = () => {
   return (
     <section className="w-full bg-white  px-4 md:px-12 lg:px-20 py-20">
-      <div className="max-w-[1280px] h-[432px] mx-auto flex flex-col lg:flex-row items-center gap-10">
+      <div className="max-w-[1280px] min-h-[432px] h-auto mx-auto flex flex-col lg:flex-row items-center gap-10">
         {/* Left: Text and CTA */}
         <div className="flex-1 max-w-[600px]">
           <h1 className="text-[40px] leading-tight font-bold text-[#000E43] font-['Inter'] mb-4">
@@ -25,7 +25,7 @@ const Hero = () => {
             <Button
               type="submit"
               variant="outline"
-              className="w-fit mb-2 px-8 h-[48px] py-4 mt-2 text-base/[24px] font-semibold"
+              className="w-fit mb-2 h-[48px] py-4 mt-2 text-base/[24px] font-semibold"
             >
               Request a Demo
             </Button>
